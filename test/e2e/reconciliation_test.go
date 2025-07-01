4efea082a8e6bb013f8a6bcf0a5b945ff8c9fa0e
fdpeiter <11605227+fdpeiter@users.noreply.github.com>
2025-07-01 10:13:45 -0300

    Update reconciliation_test.go

--- test/e2e/reconciliation_test.go
+++ test/e2e/reconciliation_test.go
@@ -31,7 +31,6 @@ var _ = Describe("ClusterResourceQuota Reconciliation", func() {
 
 	BeforeEach(func() {
 		suffix = generateTestSuffix()
-		suffix = strconv.Itoa(rand.Intn(1000000))
 		crqName = "crq-" + suffix
 		nsName = "test-ns-" + suffix
 	})
